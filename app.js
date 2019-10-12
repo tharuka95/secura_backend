d1c198a9bef73df8ea4a46669f301a479bef31b5
Tharuka Dananjana <32910424+tharuka95@users.noreply.github.com>
2019-10-12 05:30:57 +0530

    Add Cors

--- app.js
+++ app.js
@@ -6,8 +6,23 @@ var osm = require("os-monitor");
 require('loadavg-windows');
 var fileupload = require('express-fileupload');
 const netList = require('network-list');
-
+const si = require('systeminformation');
 app.use(fileupload());
+
+var cors = require ('cors');
+app.use(cors({
+    origin:['http://localhost:4200','http://127.0.0.1:4200'],
+    credentials:true
+}));
+
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+  res.header('Access-Control-Allow-Headers', true);
+  res.header('Access-Control-Allow-Credentials', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
 
 http.listen(3000, () => {
     console.log("Server running");
@@ -61,5 +76,37 @@ app.get('/devices', function(req,res,next){
             message: arr
            });
     });
+
+})
+
+
+app.get('/cpuinfo', function(req,res,next){
+    si.cpu()
+    .then(data => res.send({
+        success: 200,
+        message: data
+       }))
+    .catch(error => console.error(error));
+
+})
+
+app.get('/systeminfo', function(req,res,next){
+    si.system()
+    .then(data => res.send({
+        success: 200,
+        message: data
+       }))
+    .catch(error => console.error(error));
+
+})
+
+
+app.get('/osinfo', function(req,res,next){
+    si.osInfo()
+    .then(data => res.send({
+        success: 200,
+        message: data
+       }))
+    .catch(error => console.error(error));
 
 })
